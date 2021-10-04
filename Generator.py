e37045db2317197e20abb49bf1edbe24e78ea3f9
Season101 <sijanstudycomputers@gmail.com>
2021-10-03 23:24:29 -0500

    Working on graphs. Made Generator Generic.

--- Generator.py
+++ Generator.py
@@ -2,25 +2,21 @@ import random as rand
 
 class Generator:
 
-    def __init__(self):
+    def __init__(self, leftRange, rightRange):
         self.ranNums = {}
+        self.leftRange = leftRange
+        self.rightRange = rightRange
 
     def random_generator(self):       
-        for i in range(0,1000):
-            a = rand.randrange(-1000,1000,1)
-            if a not in self.ranNums:
-                self.ranNums[a]=1
-            else:
-                self.ranNums[a] = self.ranNums.get(a)+1 
-            print(((str)(i+1))+" :  "+(str)(a))
+        for i in range(0,10000):
+            a = rand.randrange(self.leftRange,self.rightRange,1)
+        if a not in self.ranNums:
+            self.ranNums[a]=1
+        else:
+            self.ranNums[a]= self.ranNums.get[a]+1
+
+    def get_nums(self):
         return list(self.ranNums.keys())
 
-    def print_nums(self):
-        x = self.random_generator()
-        for i in range(0,len(x)):
-            print((str)(x[i]) + " " + "count: "+(str) (self.ranNums.get(x[i])))
-
-    def plot_dist(self):
-        x_values = self.ranNums.keys()
-        y_values = self.ranNums.values()
-        return x_values,y_values
+    def get_counts(self):
+        return list(self.ranNums.values())
