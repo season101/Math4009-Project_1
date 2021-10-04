c0e80ffcef5d1ecfc8dc8d88c473375c33dee110
Season101 <sijanstudycomputers@gmail.com>
2021-10-04 07:48:35 -0500

    Fixed bugs. Working on Graphs for distribution.

--- Generator.py
+++ Generator.py
@@ -6,17 +6,20 @@ class Generator:
         self.ranNums = {}
         self.leftRange = leftRange
         self.rightRange = rightRange
+        self.random_generator()
 
     def random_generator(self):       
         for i in range(0,10000):
             a = rand.randrange(self.leftRange,self.rightRange,1)
-        if a not in self.ranNums:
-            self.ranNums[a]=1
-        else:
-            self.ranNums[a]= self.ranNums.get[a]+1
+            if a not in self.ranNums:
+                self.ranNums[a]=1
+            else:
+                self.ranNums[a]= self.ranNums.get(a)+1
 
-    def get_nums(self):
-        return list(self.ranNums.keys())
+'''
+def get_nums(self):
+    return list(self.ranNums.keys())
 
-    def get_counts(self):
-        return list(self.ranNums.values())
+def get_counts(self):
+    return list(self.ranNums.values())
+    '''
